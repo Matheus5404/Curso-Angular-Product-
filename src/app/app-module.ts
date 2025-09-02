8402f63a196f3a8786b46a5819a2ac3d05eb6323
Matheus Duarte <teusinhoduarte@gmail.com>
2025-09-02 11:04:27 -0300

    Aula 3 - Formulario Angular

---
+++ src/app/app-module.ts
@@ -0,0 +1,27 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductComponent } from './product-component/product-component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    App,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
